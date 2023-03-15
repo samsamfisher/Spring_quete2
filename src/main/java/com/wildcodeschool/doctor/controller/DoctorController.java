af0029503ab1b4857e24c15187f61100ee206ba0
samfisher <samy.perissutti@gmail.com>
2023-03-15 16:23:09 +0100

    up

---
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -0,0 +1,25 @@
+package com.wildcodeschool.doctor.controller;
+
+import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/{number}")
+    @ResponseBody
+    public Doctor doctor(@PathVariable int number) {
+        if (number == 13) {
+            return new Doctor(13, "Jodie Whittaker");
+        } else if (number > 0 && number < 13) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "See Other");
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation" + number);
+        }
+    }
+}
